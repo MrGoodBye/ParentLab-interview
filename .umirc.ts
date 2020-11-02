a97bb9911e2c7499fd3680615881bdbfa595056d
MrGoodBye <1254993267@qq.com>
2020-11-02 20:09:40 +0800

    interview

--- .umirc.ts
+++ .umirc.ts
@@ -4,10 +4,5 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-    { path: '/chat/:result?', component: '@/pages/chat' },
-    { path: '/tags', component: '@/pages/tags' },
-    { path: '/slider/:tag', component: '@/pages/slider' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
-import Chat from './chat';
+import React, { useState } from 'react';
+import { List, Image, Input } from 'antd';
+import axios from 'axios';
 import styles from './index.less';
 
 export default () => {
+  const [keyword, setKeyword] = useState('kitten');
+  const [data, setData] = useState({ photo: [], total: 100, perpage: 100 });
+  const [page, setPage] = useState(1);
+  const onSearch = async (current = page) => {
+    const result = await axios(
+      `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3e7cc266ae2b0e0d78e279ce8e361736&format=json&nojsoncallback=1&safe_search=1&page=${current}&text=${keyword}`,
+    );
+    setData(result.data.photos);
+  };
   return (
-    <div className={styles.app}>
-      <Chat />
-    </div>
+    <List
+      header={
+        <Input.Search
+          placeholder="input search text"
+          enterButton="Search"
+          size="large"
+          value={keyword}
+          onChange={e => setKeyword(e.target.value)}
+          onSearch={() => onSearch(page)}
+        />
+      }
+      grid={{ gutter: 16, column: 4 }}
+      dataSource={data.photo}
+      pagination={{
+        current: page,
+        pageSize: data.perpage,
+        pageSizeOptions: [data.perpage],
+        total: data.total,
+        onChange: current => {
+          setPage(current);
+          onSearch(current);
+        },
+      }}
+      renderItem={item => (
+        <List.Item>
+          <Image
+            src={`http://farm${item.farm}.static.flickr.com/${item.server}/${item.id}_${item.secret}.jpg `}
+          />
+        </List.Item>
+      )}
+    />
   );
 };
